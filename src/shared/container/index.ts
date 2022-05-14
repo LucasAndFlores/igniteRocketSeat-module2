b7ca5e5288f58b11781e4832c3de0e1615012820
Lucas Andrade Flores <lucas.andrade@roihunter.com>
2022-05-14 17:50:58 -0300

    feat: add folder container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,15 @@
 import { container } from "tsyringe";
 import { ICategoryRepository } from "../../modules/cars/repositories/ICategoryRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
+import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationRepository } from "../../modules/cars/infra/typeorm/repositories/SpecificationRepository";
 import { IUserRepository } from "../../modules/accounts/repositories/IUsersRepository";
-import { UserRepository } from "../../modules/accounts/repositories/implementations/UserRepository";
+import { UserRepository } from "../../modules/accounts/infra/typeorm/repositories/UserRepository";
+import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "../../modules/cars/infra/typeorm/repositories/CarsRepository";
+import { CarsImagesRepository } from "../../modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { ICarsImagesRepository } from "../../modules/cars/repositories/ICarsImageRepository";
+
 
 container.registerSingleton<ICategoryRepository>(
   "CategoriesRepository",
@@ -17,3 +22,7 @@ container.registerSingleton<ISpecificationRepository>(
 );
 
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImagesRepository)
