c76c2f62ce99bde352c70f277d73314cf291ac1a
Lucas Andrade Flores <lucas.andrade@roihunter.com>
2022-05-14 18:00:10 -0300

    feat: add create users module

---
+++ src/modules/accounts/infra/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,42 @@
+import { getRepository, Repository } from "typeorm";
+import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
+import { IUserRepository } from "../../../repositories/IUsersRepository";
+
+export class UserRepository implements IUserRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    driverLicense,
+    email,
+    name,
+    password,
+    avatar,
+    id,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      password,
+      email,
+      driver_license: driverLicense,
+      avatar,
+      id,
+    });
+
+    await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.repository.findOne({ email });
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const find = (await this.repository.findOne(id)) as User;
+    return find;
+  }
+}
